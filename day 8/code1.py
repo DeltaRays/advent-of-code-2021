79c2e2c836d6d57a382301bef78efd7e2e925a5a
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-12-16 14:46:12 +0100

    Day 8 code 1

--- day 8/code1.py
+++ day 8/code1.py
@@ -12,3 +12,9 @@ if __name__ == "__main__":
         raw_data = raw_example_data
     else:
         raw_data = raw_input_data
+    parsed_data = raw_data.split("\n")
+    data_we_care_about = [line.split(" | ")[-1].strip().split() for line in parsed_data]
+    flattened_data = [word for sublist in data_we_care_about for word in sublist]
+    # 2, 3, 4, 7 are the number of different segments for 1, 4, 7, 8
+    #                                                     2, 4, 3, 7
+    print(f"Total: {len([word for word in flattened_data if len(word) in (2, 3, 4, 7)])}")
