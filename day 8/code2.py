01037e5ef088a75a15ac673ff9d8715952b90cef
DeltaRays <42403698+DeltaRays@users.noreply.github.com>
2021-12-17 10:36:13 +0100

    work on day2

--- day 8/code2.py
+++ day 8/code2.py
@@ -6,10 +6,50 @@ if __name__ == "__main__":
 
     # A variable used to choose which one of the two to use
     # Example data is used to see if the code works
-    use_example_data = False
+    use_example_data = True
 
     if use_example_data:
         raw_data = raw_example_data
     else:
         raw_data = raw_input_data
-
+    lines = raw_data.split("\n")
+    possible_segments = [c for c in "abcdefg"]
+    for line in lines:
+        corrispondence = {
+            1: None,
+            2: None,
+            3: None,
+            4: None,
+            5: None,
+            6: None,
+            7: None,
+            8: None,
+            9: None
+        }
+        first_part, second_part = line.split(" | ", 2)
+        inpt: list[str] = first_part.split(" ")
+        output: list[str] = second_part.split(" ")
+        # Loop to find which segments correspond to what
+        for word in inpt:
+            # 2, 3, 4, 7 are the number of different segments for 1, 4, 7, 8
+            #                                                     2, 4, 3, 7
+            # Lengths: 1 2 3 4 5 6 7 8 9 0
+            #          2 5 5 4 5 6 3 7 6 6
+            if len(word) == 2:
+                corrispondence[1] = word
+                inpt.remove(word)
+            elif len(word) == 4:
+                corrispondence[4] = word
+                inpt.remove(word)
+            elif len(word) == 3:
+                corrispondence[7] = word
+                inpt.remove(word)
+            elif len(word) == 7:
+                corrispondence[8] = word
+                inpt.remove(word)
+        for word in inpt:
+            if len(word) == 5:
+                # If there are exactly 2 duplicate items it's a 3
+                if len(set([c for c in word] + [c for c in corrispondence[1]])) == 5:
+                    corrispondence[3] = word
+                    inpt.remove(word)
